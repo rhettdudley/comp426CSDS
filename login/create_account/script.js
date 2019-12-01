09b6d515df7fe6e6b3bbc7d0b579d2f8c73e5c5e
Will Benson <willbenson974@gmail.com>
2019-12-01 13:21:43 -0500

    account creation

--- login/create_account/script.js
+++ login/create_account/script.js
@@ -4,4 +4,38 @@ $(function() {
     $cancel.on('click', () => {
         window.location.replace("http://localhost:3000/login");
     })
+
+    const $submit = $('#submit');
+    $submit.on('click', async () => {
+
+        let $first = $('#first');
+        let $last = $('#last');
+        let $username = $('#username');
+        let $password = $('#password');
+
+        console.log($first.val());
+
+        const r = await axios({
+            method: 'POST',
+            url: 'http://localhost:3000/account/create',
+            data: {
+                "name": $username.val(),
+                "pass": $password.val(),
+                "data": {
+                    "firstname": $first.val(),
+                    'lastname': $last.val(),
+                    'favbar': 'Bobs',
+                }
+            }
+        });
+
+        r.then(response => {
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+
+    });
+    
+
 });
