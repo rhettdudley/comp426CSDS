51f890def42cf494956edb3fdcf2d25777b24739
Rhett Dudley <rhettdudley@rhetts-air.home>
2019-12-12 00:02:45 -0500

    autocomplete

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -75,7 +75,7 @@ function buildLocationList(data) {
     }
 }
 
-$(function() {
+$(async function() {
     const $reset = $('#All');
     $reset.click(function(e) {
         e.preventDefault();
@@ -196,7 +196,21 @@ $(function() {
         buildLocationDay("sunday", stores);
     })
 
+
+    //const $autocomplete = $('#autoBox');
+    var barsAC = [];
+    for (i = 0; i < stores.features.length; i++) {
+      var currentFeature = stores.features[i].properties.name;
+      barsAC.push(currentFeature);
+    }
+
+    autocomplete(document.getElementById("autoBox"), barsAC);
+
+
+    await getPrivateSpecials();
 })
+
+
 
 function buildLocationDay(day, data) {
     // Iterate through the list of stores
@@ -345,3 +359,141 @@ function buildLocationDay(day, data) {
       });
     }
 }
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  // debounce event listenes
+
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+     
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+var textbox = $('#autobox')
+const myHandler = (e) => {
+
+}
+
+
+document.getElementById("autoBox").addEventListener("keydown", debounced(500, autocomplete));
+//const eHandler = $('#autobox')
+
+function debounced(delay, fn) { 
+  let timerID
+  return function (...args) {
+    if (timerID) {
+      clearTimeout(timerID);
+    }
+    timerID = setTimeout (() => {
+      fn(...args);
+      timerID = null;
+    }, delay);
+  }
+}
+
+async function getPrivateSpecials() {
+  const jwt = localStorage.getItem('jwt');
+    //console.log(jwt);
+    axios.get("http://localhost:3000/private",
+      {headers: { Authorization: `Bearer ` + jwt }
+    })
+    .then(res => {
+      console.log(res);
+      // var favbar = $('#favoritebar');
+      // favbar.html('<h2 class="subtitle is-5" id = "favoritebar">'+ 'Your favorite bar is: ' + res.data.result + '</h2>');
+      
+    })
+    .catch(err => console.log(err));
+   
+}
+
